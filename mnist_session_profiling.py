c1d880e168521baaa7cbdf3cc6f4ca55c90eea8b
Mikalai Drabovich <drabovich@gmail.com>
2018-01-26 12:15:14 -0800

    enable memory profiling for session.run() generated traces

--- mnist_session_profiling.py
+++ mnist_session_profiling.py
@@ -87,17 +87,20 @@ def main(_):
         loss, train_accuracy  = sess.run([cross_entropy, accuracy], feed_dict={x: batch[0], y_: batch[1], keep_prob: 1.0})
         validation_accuracy = accuracy.eval(feed_dict={x: mnist.validation.images, y_: mnist.validation.labels, keep_prob: 1.0})
         print('processed %d/%d images, batch %d/%d, epoch %g/%g, train acc: %g, val acc:%g, loss:%g' % (i*batch_size, total_images_to_process, i, total_batches_to_process, i*batch_size/num_train_images, total_epochs_to_train, train_accuracy, validation_accuracy, loss))
-      if i % 1000 == 0:
-        save_path = saver.save(sess, "./train_saver_1/")
-        dir_to_export_model = saved_model_export_utils.get_timestamped_export_dir('./train_saver_2/')
-        simple_save.simple_save(sess,  dir_to_export_model, {'x':x}, {'y_conv':y_conv})
-        print("Model saved to file: %s, exported to %s" % (save_path, dir_to_export_model))
-      if i % 917 == 0:
+      else:
         sess.run(train_step, feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5}, options=options, run_metadata=run_metadata)
-        fetched_timeline = timeline.Timeline(run_metadata.step_stats)
-        chrome_trace = fetched_timeline.generate_chrome_trace_format()
-        with open('timeline_02_step_%d.json' % i, 'a') as f:
-          f.write(chrome_trace)
+          
+        if i % 1000 == 0:
+          save_path = saver.save(sess, "./train_saver_1/")
+          dir_to_export_model = saved_model_export_utils.get_timestamped_export_dir('./train_saver_2/')
+          simple_save.simple_save(sess,  dir_to_export_model, {'x':x}, {'y_conv':y_conv})
+          print("Model saved to file: %s, exported to %s" % (save_path, dir_to_export_model))
+          
+        if i % 917 == 0:
+          fetched_timeline = timeline.Timeline(run_metadata.step_stats)
+          chrome_trace = fetched_timeline.generate_chrome_trace_format(show_memory=True)
+          with open('timeline_with_tensors_step_%d.json' % i, 'a') as f:
+            f.write(chrome_trace)
 
     print('test accuracy %g' % accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
 if __name__ == '__main__':
